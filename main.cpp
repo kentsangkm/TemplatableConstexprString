508d0edea560def44ddfb7c951c5e879424fcf20
Kenman <kentsangkm@users.noreply.github.com>
2018-07-16 22:52:23 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "ConstexprString.hpp"
+
+int main(int argc, char *argv[])
+{
+    // https://stackoverflow.com/questions/1826464/c-style-strings-as-template-arguments
+    constexprStr("This is a very long text. You can only store the first 64 bytes string in one marco call. ") longStr1;
+    constexprStr("If you wanna have a longer string, you can just do a decltype(str + str)") longStr2;
+        
+    std::cout << longStr1.to_string() << std::endl;
+    std::cout << std::to_string(longStr2) << std::endl;
+    std::cout << longStr1 << std::endl;
+    std::cout << longStr2 << std::endl;
+    std::cout << (longStr1 + longStr2) << std::endl;
+}
